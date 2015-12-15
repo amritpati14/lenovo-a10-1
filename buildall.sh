79c1a023daef8a2f551fd3d924062ba220677f4a
Durand John Miller <durand.miller@southpatron.com>
2015-12-16 00:40:08 +0200

    Further progress along the line

--- buildall.sh
+++ buildall.sh
@@ -12,28 +12,69 @@ fi;
 
 source ${DIR}/config
 
+BUILDDIR=${DIR}/${BUILD}
+
+RKFLASHTOOL=${DIR}/submodules/rkflashtool/rkflashtool
+
+SHELL=$(which bash)
 
 
-git submodules init
+git submodule init
+
+mkdir -p ${BUILDDIR}
+mkdir -p ${BUILDDIR}/initramfs
 
 
 # Compile rkflashtool
-cd ${DIR}/submodules/rkflashtool && make && cd -
+( cd ${DIR}/submodules/rkflashtool && \
+	make && \
+	cd - )
 
 # Compile uboot
 
-echo ${ARCH} ${CROSS_COMPILE}
+( cd ${DIR}/submodules/u-boot-rockchip && \
+	git checkout u-boot-rk3188-sdcard && \
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make rk30xx_config && \
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make rk30xx && \
+	cd - )
 
-exit 0;
-         
-
-
-cd ${DIR}/submodules/
+cp -f ${DIR}/submodules/u-boot-rockchip/RK3188Loader_uboot.bin ${BUILDDIR}
 
 
 echo "Configuring and compiling the kernel and modules"
 
-cd ${DIR}/kernel && make rk3188_flex10_defconfig && make && make modules && cd -
+( cd ${DIR}/kernel && \
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make rk3188_flex10_defconfig && \
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make && \
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make modules && \
+	cd - )
+
+
+#cp -f kernel/arch/arm/boot/
+
+
+# Creating the initramfs
+
+(
+	mkdir -p ${BUILDDIR}/initramfs/{bin,sbin,etc,proc,sys,newroot}
+	touch ${BUILDDIR}/initramfs/etc/mdev.conf
+	cp -rf ${DIR}/parts/initramfs/* ${BUILDDIR}/initramfs/
+	find . | cpio -H newc -o > ${BUILDDIR}/initramfs.cpio
+	cat ${BUILDDIR}/initramfs.cpio | gzip > ${BUILDDIR}/initramfs.igz
+	cd -
+)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
