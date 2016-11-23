92eb61999ec0d261a5ef568b519619842178f201
Durand John Miller <durand.miller@southpatron.com>
2016-11-23 23:54:28 +0200

    Fix accidently script change

--- scripts/dokernelextra.sh
+++ scripts/dokernelextra.sh
@@ -68,7 +68,9 @@ echo "Configuring and compiling the kernel and modules"
 	cp ${KCONFIG} ${KERNEL}/.config && \
 	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make && \
 	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make modules && \
-	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make headers_install && \
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make modules_install INSTALL_MOD_PATH=${INITRAMFS} && \
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make firmware_install INSTALL_MOD_PATH=${INITRAMFS} && \
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make headers_install INSTALL_HDR_PATH=${INITRAMFS}/usr/ && \
 	cd - )
 
 
